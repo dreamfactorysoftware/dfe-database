cb13a5c4d83fe45ca57f5841f75f339b31043ebf
Jerry Ablan <jerryablan@dreamfactory.com>
2014-12-01 00:30:22 -0500

    model added for Deactivation tables

---
+++ src/Models/Deploy/InstanceArchive.php
@@ -0,0 +1,18 @@
+<?php
+namespace DreamFactory\Library\Fabric\Database\Models\Deploy;
+
+/**
+ * instance_arch_t
+ */
+class InstanceArchive extends Instance
+{
+    //******************************************************************************
+    //* Members
+    //******************************************************************************
+
+    /**
+     * @type string The table name
+     */
+    protected $table = 'instance_arch_t';
+
+}
