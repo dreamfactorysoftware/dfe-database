1166730bde918ed3105fc6babcb57f9c89227815
Jerry Ablan <jerryablan@dreamfactory.com>
2015-02-23 15:48:01 -0500

    Added phpdoc to User model

--- src/Models/Auth/User.php
+++ src/Models/Auth/User.php
@@ -4,7 +4,38 @@ namespace DreamFactory\Library\Fabric\Database\Models\Auth;
 use DreamFactory\Library\Fabric\Database\Models\AuthModel;
 
 /**
- * fabric_auth.user_t
+ * user_t table
+ *
+ * @property int    drupal_id
+ * @property string api_token_text
+ * @property string first_name_text
+ * @property string last_name_text
+ * @property string display_name_text
+ * @property string email_addr_text
+ * @property string password_text
+ * @property string drupal_password_text
+ * @property int    owner_id
+ * @property int    owner_type_nbr
+ * @property string company_name_text
+ * @property string title_text
+ * @property string city_text
+ * @property string state_province_text
+ * @property string country_text
+ * @property string postal_code_text
+ * @property string phone_text
+ * @property string fax_text
+ * @property int    opt_in_ind
+ * @property int    agree_ind
+ * @property string valid_email_hash_text
+ * @property int    valid_email_hash_expire_time
+ * @property string valid_email_date
+ * @property string recover_hash_text
+ * @property int    recover_hash_expire_time
+ * @property string last_login_date
+ * @property string last_login_ip_text
+ * @property int    admin_ind
+ * @property string storage_id_text
+ * @property int    activate_ind
  */
 class User extends AuthModel
 {
