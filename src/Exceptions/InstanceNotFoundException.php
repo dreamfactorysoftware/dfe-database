f100bab0a95bd699624f3bcaff3e5848b005c3ed
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-15 15:25:32 -0400

    Current work checkpoint. See diff for details.

---
+++ src/Exceptions/InstanceNotFoundException.php
@@ -0,0 +1,10 @@
+<?php namespace DreamFactory\Enterprise\Database\Exceptions;
+
+use Exception;
+
+/**
+ * For when an instance isn't found
+ */
+class InstanceNotFoundException extends \Exception
+{
+}
---
+++ src/Traits/InstanceValidation.php
@@ -0,0 +1,52 @@
+<?php
+namespace DreamFactory\Enterprise\Database\Traits;
+
+use DreamFactory\Enterprise\Common\Contracts\InstanceContainer;
+use DreamFactory\Enterprise\Database\Exceptions\InstanceNotFoundException;
+use DreamFactory\Enterprise\Database\Models\Instance;
+
+/**
+ * A trait for validating instances
+ */
+trait InstanceValidation
+{
+    //*************************************************************************
+    //* Methods
+    //*************************************************************************
+
+    /**
+     * @param string|Instance $instanceId
+     *
+     * @return \DreamFactory\Enterprise\Database\Models\Instance
+     * @throws \DreamFactory\Enterprise\Database\Exceptions\InstanceNotFoundException
+     */
+    protected function _validateInstance( $instanceId )
+    {
+        if ( $instanceId instanceof Instance )
+        {
+            return $instanceId;
+        }
+
+        if ( $instanceId instanceOf InstanceContainer )
+        {
+            return $instanceId->getInstance();
+        }
+
+        if ( !is_string( $instanceId ) )
+        {
+            throw new InstanceNotFoundException( $instanceId );
+        }
+
+        try
+        {
+            $instanceId = Instance::sanitizeName( $instanceId );
+
+            return Instance::byNameOrId( $instanceId )->firstOrFail();
+        }
+        catch ( \Exception $_ex )
+        {
+            throw new InstanceNotFoundException( $instanceId );
+        }
+    }
+
+}
