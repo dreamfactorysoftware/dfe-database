a0bd9b638bdb1adfd048ebb05e0d7f8ab4f802d3
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-10 20:15:16 -0400

    Current work checkpoint. See diff for details.

--- src/Models/Mount.php
+++ src/Models/Mount.php
@@ -86,43 +86,30 @@ class Mount extends EnterpriseModel
     public function getFilesystem($path = null, $tag = null, $options = [], $nameOnly = false)
     {
         $_diskName = null;
+        $_mountConfig = $this->config_text;
 
-        if (null === ($_info = IfSet::get($this->config_text, 'disk'))) {
-            if (null === ($_info = IfSet::get($this->config_text, 'connection'))) {
-                throw new \RuntimeException('No "disk" or "connection" configured for mount "' . $this->mount_id_text . '".');
-            }
+        if (null === ($_diskName = IfSet::get($_mountConfig, 'disk'))) {
+            throw new \RuntimeException('No "disk" configured for mount "' . $this->mount_id_text . '".');
         }
 
-        //  If it's a string, it should be pre-configured
-        if (is_string($_info)) {
-            if (null === ($_config = config('flysystem.connections.' . $_info))) {
-                throw new \RuntimeException('No pre-configured mount "' . $_info . '" found.');
-            }
-
-            return Mounter::mount($_diskName, array_merge($options, ['prefix' => $path, 'tag' => $tag]));
-        }
-
-        //  If it's not an array, bail
-        if (!is_array($_info)) {
-            throw new \RuntimeException('Invalid configuration for mount disk "' . $_info . '".');
-        }
+        return Mounter::mount($_diskName, array_merge($options, ['prefix' => $path, 'tag' => $tag]));
 
         //@todo Dynamically configured disk is not yet supported because of a config provider issue
 
-        $_config = $_info;
-
-        if (!$_diskName && null === ($_diskName = IfSet::get($_config, 'name'))) {
-            $_diskName = 'mount-temp-' . microtime(true);
-        }
-
-        !isset($_config['driver']) && $_config['driver'] = 'local';
-        !isset($_config['path']) && isset($_config['root']) && $_config['path'] = $_config['root'];
-        unset($_config['root'], $_config['name']);
-
-        if ($nameOnly) {
-            return $_diskName;
-        }
-
-        return Mounter::mount($_diskName, array_merge($_config, $options, ['prefix' => $path, 'tag' => $tag]));
+//        $_config = $_info;
+//
+//        if (!$_diskName && null === ($_diskName = IfSet::get($_config, 'name'))) {
+//            $_diskName = 'mount-temp-' . microtime(true);
+//        }
+//
+//        !isset($_config['driver']) && $_config['driver'] = 'local';
+//        !isset($_config['path']) && isset($_config['root']) && $_config['path'] = $_config['root'];
+//        unset($_config['root'], $_config['name']);
+//
+//        if ($nameOnly) {
+//            return $_diskName;
+//        }
+//
+//        return Mounter::mount($_diskName, array_merge($_config, $options, ['prefix' => $path, 'tag' => $tag]));
     }
 }
