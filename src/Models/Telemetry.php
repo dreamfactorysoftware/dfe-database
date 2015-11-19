336088cb4291ebcf7e88f0c5608aad30bd0d4110
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-18 19:53:23 -0500

    Added new model Telemetry for the new table telemetry_t

--- src/Models/Telemetry.php
+++ src/Models/Telemetry.php
@@ -1,5 +1,7 @@
 <?php namespace DreamFactory\Enterprise\Database\Models;
 
+use Carbon\Carbon;
+
 /**
  * telemetry_t
  *
@@ -22,4 +24,24 @@ class Telemetry extends EnterpriseModel
         'create_date' => 'datetime',
         'lmod_date'   => 'datetime',
     ];
+
+    //******************************************************************************
+    //* Methods
+    //******************************************************************************
+
+    /**
+     * @param string $providerId The provider id
+     * @param array  $data       The data gathered
+     * @param string $date       The date of the gather
+     *
+     * @return static
+     */
+    public static function storeTelemetry($providerId, array $data, $date = null)
+    {
+        return static::create([
+            'provider_id_text' => $providerId,
+            'data_text'        => $data,
+            'gather_date'      => $date ?: Carbon::now(),
+        ]);
+    }
 }
